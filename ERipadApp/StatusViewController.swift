61d968392a351024bb7bf245f9da0a4c18f5f31d
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-12-21 06:49:00 +0900

    リアルタイム監視によるCellの色を変化させるコードの実装中。

--- ERipadApp/StatusViewController.swift
+++ ERipadApp/StatusViewController.swift
@@ -15,6 +15,7 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
     var hall: HallData!
     var broadcast: BroadcastData!
     var broadArray: [BroadcastData] = []
+    var listener: ListenerRegistration?
     
     @IBAction func emergencyReadyButton(_ sender: Any) {
         let ref = Firestore.firestore().collection("emergencies").document(hall.id)
@@ -104,19 +105,63 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
 //        cell.layer.borderColor = UIColor.black.cgColor
         cell.layer.borderWidth = 5.0
         if broadArray[indexPath.row].level == 1 {
-            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1).cgColor
         } else if broadArray[indexPath.row].level == 2 {
-            cell.layer.borderColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1).cgColor
+            cell.layer.borderColor = UIColor(red: 0.35, green: 0.35, blue: 0.35, alpha: 1).cgColor
         } else if broadArray[indexPath.row].level == 3 {
             cell.layer.borderColor = UIColor(red: 0.50, green: 0.50, blue: 0.50, alpha: 1).cgColor
         } else if broadArray[indexPath.row].level == 4 {
-            cell.layer.borderColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1).cgColor
-        } else if broadArray[indexPath.row].level == 5 {
-            cell.layer.borderColor = UIColor(red: 0.05, green: 0.05, blue: 0.05, alpha: 1).cgColor
+            cell.layer.borderColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1).cgColor
+        } else {
+            cell.layer.borderColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1).cgColor
+        }
+        
+        func viewWillAppear(_ animated: Bool) {
+            listener = Firestore.firestore().collection("areas").document(area.id).collection("halls").document(hall.id).collection("broadcasts").addSnapshotListener { documentSnapshot, error in
+                if let error = error {
+                    print("ドキュメントの取得に失敗しました",error)
+                } else {
+                    self.broadArray = documentSnapshot!.documents.map { document in
+                        let broadcast = BroadcastData(document: document)
+                        return broadcast
+                    }
+                }
+                
+                if self.broadcast.status == 0 {
+                    cell.backgroundColor = .white
+                } else if self.broadcast.status == 1 {
+                    cell.backgroundColor = .cyan
+                } else if self.broadcast.status == 2 {
+                    cell.backgroundColor = .yellow
+                } else if self.broadcast.status == 3 {
+                    cell.backgroundColor = .green
+                } else if self.broadcast.status == 4 {
+                    cell.backgroundColor = .orange
+                }
+                
+                
+            }
         }
         
         return cell
     }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        listener = Firestore.firestore().collection("areas").document(area.id).collection("halls").document(hall.id).collection("broadcasts").addSnapshotListener { documentSnapshot, error in
+//                   if let error = error {
+//                       print("ドキュメントの取得に失敗しました",error)
+//                   } else {
+//                       self.broadArray = documentSnapshot!.documents.map { document in
+//                           let broadcast = BroadcastData(document: document)
+//                           return broadcast
+//
+//                           if self.broadcast.status == 1 {
+//
+//                               }
+//                           }
+//                       }
+//                   }
+//    }
     
     
 
