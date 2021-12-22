e535672025557ceec0a1693047fc9ce815d1af07
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-12-22 18:30:49 +0900

    なぜか上手くいっているファイル

--- ERipadApp/StatusViewController.swift
+++ ERipadApp/StatusViewController.swift
@@ -15,7 +15,6 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
     var hall: HallData!
     var broadcast: BroadcastData!
     var broadArray: [BroadcastData] = []
-    var listener: ListenerRegistration?
     
     @IBAction func emergencyReadyButton(_ sender: Any) {
         let ref = Firestore.firestore().collection("emergencies").document(hall.id)
@@ -49,7 +48,6 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
     @IBOutlet weak var emergencyReadyButton: UIButton!
     @IBOutlet weak var emergencyEndButton: UIButton!
     @IBOutlet weak var emergencyReadyFakeButton: UIButton!
-    
     @IBOutlet weak var collectionView: UICollectionView!
     
     
@@ -69,23 +67,24 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
         }
         
         // Do any additional setup after loading the view.
-        if Auth.auth().currentUser != nil {
-            let broadRef = Firestore.firestore().collection("areas").document(area!.id).collection("halls").document(hall!.id).collection("broadcasts").order(by: "code")
-            broadRef.getDocuments() { (querySnapshot, error) in
-                if let error = error {
-                    print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
-                    return
-                }
-                self.broadArray = querySnapshot!.documents.map { document in
-                    let broadcast = BroadcastData(document: document)
-                    print("DEBUG_PRINT: document取得 \(broadcast.name)")
-
-                    return broadcast
-
-                }
-                self.collectionView.reloadData()
-            }
-        }
+//
+//        if Auth.auth().currentUser != nil {
+//            let broadRef = Firestore.firestore().collection("areas").document(area!.id).collection("halls").document(hall!.id).collection("broadcasts").order(by: "code")
+//            broadRef.getDocuments() { (querySnapshot, error) in
+//                if let error = error {
+//                    print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
+//                    return
+//                }
+//                self.broadArray = querySnapshot!.documents.map { document in
+//                    let broadcast = BroadcastData(document: document)
+//                    print("DEBUG_PRINT: document取得 \(broadcast.name)")
+//
+//                    return broadcast
+//
+//                }
+//                self.collectionView.reloadData()
+//            }
+//        }
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -99,80 +98,77 @@ class StatusViewController: UIViewController, UICollectionViewDelegate, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         
+        let broadcast = broadArray[indexPath.row]
         let label = cell.contentView.viewWithTag(1) as? UILabel
-        label?.text = broadArray[indexPath.row].name
+        label?.text = broadcast.name
         
-//        cell.layer.borderColor = UIColor.black.cgColor
+        
         cell.layer.borderWidth = 5.0
-        if broadArray[indexPath.row].level == 1 {
+        if broadcast.level == 1 {
             cell.layer.borderColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1).cgColor
-        } else if broadArray[indexPath.row].level == 2 {
+        } else if broadcast.level == 2 {
             cell.layer.borderColor = UIColor(red: 0.35, green: 0.35, blue: 0.35, alpha: 1).cgColor
-        } else if broadArray[indexPath.row].level == 3 {
+        } else if broadcast.level == 3 {
             cell.layer.borderColor = UIColor(red: 0.50, green: 0.50, blue: 0.50, alpha: 1).cgColor
-        } else if broadArray[indexPath.row].level == 4 {
+        } else if broadcast.level == 4 {
             cell.layer.borderColor = UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1).cgColor
         } else {
             cell.layer.borderColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1).cgColor
         }
         
-        func viewWillAppear(_ animated: Bool) {
-            listener = Firestore.firestore().collection("areas").document(area.id).collection("halls").document(hall.id).collection("broadcasts").addSnapshotListener { documentSnapshot, error in
-                if let error = error {
-                    print("ドキュメントの取得に失敗しました",error)
-                } else {
-                    self.broadArray = documentSnapshot!.documents.map { document in
-                        let broadcast = BroadcastData(document: document)
-                        return broadcast
-                    }
-                }
-                
-                if self.broadcast.status == 0 {
-                    cell.backgroundColor = .white
-                } else if self.broadcast.status == 1 {
-                    cell.backgroundColor = .cyan
-                } else if self.broadcast.status == 2 {
-                    cell.backgroundColor = .yellow
-                } else if self.broadcast.status == 3 {
-                    cell.backgroundColor = .green
-                } else if self.broadcast.status == 4 {
-                    cell.backgroundColor = .orange
-                }
-                
-                
-            }
+        if broadcast.status == 0 {
+            cell.backgroundColor = .white
+        }else if broadcast.status == 1 {
+            cell.backgroundColor = .cyan
+        }else if broadcast.status == 2 {
+            cell.backgroundColor = .yellow
+        }else if broadcast.status == 3 {
+            cell.backgroundColor = .green
+        }else if broadcast.status == 4 {
+            cell.backgroundColor = .orange
+        }else if broadcast.status == 5 {
+            cell.backgroundColor = .purple
         }
         
         return cell
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        listener = Firestore.firestore().collection("areas").document(area.id).collection("halls").document(hall.id).collection("broadcasts").addSnapshotListener { documentSnapshot, error in
-//                   if let error = error {
-//                       print("ドキュメントの取得に失敗しました",error)
-//                   } else {
-//                       self.broadArray = documentSnapshot!.documents.map { document in
-//                           let broadcast = BroadcastData(document: document)
-//                           return broadcast
-//
-//                           if self.broadcast.status == 1 {
-//
-//                               }
-//                           }
-//                       }
-//                   }
-//    }
+    var cellColor = 0
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        if Auth.auth().currentUser != nil {
+            let broadRef = Firestore.firestore().collection("areas").document(area!.id).collection("halls").document(hall!.id).collection("broadcasts").order(by: "code")
+            broadRef.addSnapshotListener() { (querySnapshot, error) in
+                if let error = error {
+                    print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
+                    return
+                }
+                self.broadArray = querySnapshot!.documents.map { document in
+                    let broadcast = BroadcastData(document: document)
+                    print("DEBUG_PRINT: document取得 \(broadcast.status)")
+                    
+                    return broadcast
+                    
+                }
+                self.collectionView.reloadData()
+            }
+        }
+        
     }
-    */
+    
+
+
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 
 }
